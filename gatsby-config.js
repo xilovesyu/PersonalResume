d51ae9a987cf8b8482bc051bf73eba26669e765d
jiaxiang.xi <jiaxiang.xi@eisgroup.com>
2019-11-14 14:02:30 +0800

    Added favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
 
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     {
       resolve: "gatsby-source-filesystem",
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Header from '../components/Header'
 import Container from "../components/Container";
-
+import { Helmet } from "react-helmet"
+import icon from "../../static/favicon.ico"
 export default class Index extends React.Component {
     render() {
         return <>
+            <Helmet defer={false} defaultTitle={'Personal Resume'}>
+                <link rel="shortcut icon" type="image/x-icon" href={icon}/>
+            </Helmet>
             <Header />
             <Container />
             </>
